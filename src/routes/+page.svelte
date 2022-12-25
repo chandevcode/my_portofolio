1f3484b3c8eb19eb6b2b4627a43cc33384d9c118
chan <baguschandradp@gmail.com>
2022-12-25 18:54:34 +0800

    profile

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<div class="flex justify-center items-center h-screen w-screen">
+	<div
+		class="flex justify-around content-center text-center  bg-gray-600 w-full p-10 rounded-md max-w-2xl "
+	>
+		<div class="mx-5">
+			<div class="avatar">
+				<div class="w-56 rounded-full ring ring-accent ring-offset-base-100 ring-offset-2">
+					<img
+						src="https://media.licdn.com/dms/image/C4E03AQFulLzsVy0hrQ/profile-displayphoto-shrink_800_800/0/1630538915553?e=1677715200&v=beta&t=IsSZ6BnbmsUhJu9ePG_3vrQKvg4tsveiLmFljA2Whe4"
+						alt="my profile"
+					/>
+				</div>
+			</div>
+		</div>
+		<!-- Content Here  -->
+		<div class="text-start">
+			<h1 class="text-3xl font-bold text-gray-300">Putu Chandra</h1>
+			<h4 class=" font-bold mt-3 ">About Me</h4>
+			<p class="">I'am a Web Developer. Let's talk about current web technology</p>
+			<div class="flex mt-10 gap-5 ">
+				<a
+					href="https://www.linkedin.com/in/putu-chandra-3ba1941a9/"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="btn btn-primary"
+				>
+					linkedin</a
+				>
+				<a href="#" target="_blank" rel="noopener noreferrer" class="btn btn-accent"> Project</a>
+				<a
+					href="https://github.com/chanlevelup"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="btn btn-neutral"
+				>
+					Github</a
+				>
+			</div>
+		</div>
+	</div>
+</div>
